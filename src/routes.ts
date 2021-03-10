385777596c9c64b2eeb5842e61f0fdf7315a0f83
João Santos <joaopfs@outlook.com.br>
2021-03-10 20:06:25 -0300

    modified routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,8 @@ const userController = new UserController();
 const surveysController = new SurveyController();
 
 router.post("/users", userController.create);
+
 router.post("/surveys", surveysController.create);
+router.get("/surveys", surveysController.show);
 
 export { router };
